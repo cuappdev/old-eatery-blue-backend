4424b47ec4e0dac488a2407cb0116e91fca016be
skyeslattery <dss354@cornell.edu>
2026-01-07 16:15:03 -0500

    switch base url to new dining api url

--- src/eatery/util/constants.py
+++ src/eatery/util/constants.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 from eatery.datatype.Eatery import EateryID
 
-CORNELL_DINING_URL = "https://now.dining.cornell.edu/api/1.0/dining/eateries.json"
+CORNELL_DINING_URL = "https://admin-now.dining.cornell.edu/api/1.0/dining/eateries.json"
 CORNELL_VENDOR_URL = (
     "https://vendor-api-extra.scl.cornell.edu/api/external/location-count"
 )
